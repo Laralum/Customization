6fd02828be5f1111611144ef89f788e24bb76bbd
Erik Campobadal <soc@erik.cat>
2017-02-21 08:28:33 +0100

    Tweaks

--- src/Views/settings.blade.php
+++ src/Views/settings.blade.php
@@ -4,7 +4,7 @@
 <div uk-grid>
     <div class="uk-width-1-1@s uk-width-1-5@l"></div>
     <div class="uk-width-1-1@s uk-width-3-5@l">
-        <form method="POST" action="{{ route('laralum::customization.index.update') }}">
+        <form class="uk-form-horizontal" method="POST" action="{{ route('laralum::customization.index.update') }}">
             {{ csrf_field() }}
             <fieldset class="uk-fieldset">
 
@@ -24,11 +24,13 @@
 
                 <div class="uk-margin">
                     <label class="uk-form-label">@lang('laralum_customization::general.navbar_color')</label>
-                    <input value="{{ old('navbar_color', $customization->navbar_color ? $customization->navbar_color : '') }}" name="navbar_color" class="uk-input" type="text" placeholder="@lang('laralum_customization::general.navbar_color_ph')">
-                    <small class="uk-text-meta">@lang('laralum_customization::general.navbar_color_hp')</small>
+                    <div class="uk-form-controls">
+                        <input value="{{ old('navbar_color', $customization->navbar_color ? $customization->navbar_color : '') }}" name="navbar_color" class="uk-input" type="text" placeholder="@lang('laralum_customization::general.navbar_color_ph')">
+                        <small class="uk-text-meta">@lang('laralum_customization::general.navbar_color_hp')</small>
+                    </div>
                 </div>
 
-                <div class="uk-margin">
+                <div class="uk-margin uk-align-right">
                     <button type="submit" class="uk-button uk-button-primary">
                         <span class="ion-forward"></span>&nbsp; @lang('laralum_customization::general.save')
                     </button>
